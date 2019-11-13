8ac014315b8b74d0d59a05a8f8fb115fa601e6ea
GabbyJ <GabbyJ424@gmail.com>
2019-11-13 16:25:20 -0600

    Add files via upload

---
+++ build/precache-manifest.d2742f22872653bb2afefebde6821f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4785eead778274d835449dd832dec60",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "193ad1255808aab46a5f",
+    "url": "/First-React-App/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "846d6604dcdccd2389d2",
+    "url": "/First-React-App/static/js/2.e77f593c.chunk.js"
+  },
+  {
+    "revision": "193ad1255808aab46a5f",
+    "url": "/First-React-App/static/js/main.500a6828.chunk.js"
+  },
+  {
+    "revision": "93808d303d9fb495f8c1",
+    "url": "/First-React-App/static/js/runtime-main.fa2ebac6.js"
+  }
+]);
